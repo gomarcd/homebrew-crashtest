dd31bc415fa4a92571e8adb93979b75f193fddca
gomarcd <git@gomarcd.dev>
2025-04-25 07:22:18 -0600

    fix path

--- Casks/crashtest.rb
+++ Casks/crashtest.rb
@@ -2,7 +2,7 @@ cask "crashtest" do
   version "0.0.10"
   sha256 "fd0e883e123d9fe02efa0dd4d5e560b92d4c2ba97a7c9b46308f5ab3d7170ddc"
 
-  url "https://github.com/gomarcd/crashtest/releases/download/v#{version}/Crashtest_#{version}_universal.dmg",
+  url "https://github.com/gomarcd/crashtest/releases/download/v#{version}/Crashtest_2025.#{version}_universal.dmg",
       verified: "github.com/gomarcd/crashtest/"
   name "Crashtest"
   desc "Fast, minimal API tool"
